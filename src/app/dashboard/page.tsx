0a0fc137bc756d74a71fd6b8c633563a6c25cb40
Shashank Singh <sshubham.5690@gmail.com>
2024-05-31 14:45:11 +0530

    dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,26 +1,22 @@
-// // import Dashboard from '@/components/Dashboard'
+// import Dashboard from '@/components/Dashboard'
 
 
-// import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
-// import { redirect } from 'next/navigation'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
 
-// const Page = async () => {
-//   const { getUser } = getKindeServerSession()
-//   const user = getUser()
+const Page = async () => {
+  const { getUser } = getKindeServerSession()
+  const user = getUser()
 
-//   // if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
+  // if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
-//   // const dbUser = await db.user.findFirst({
-//   //   where: {
-//   //     id: user.id
-//   //   }
-//   // })
+  // const dbUser = await db.user.findFirst({
+  //   where: {
+  //     id: user.id
+  //   }
+  // })
 
-//   if(!dbUser) redirect('/auth-callback?origin=dashboard')
+ 
+}
 
-  
-
-//   return <Dashboard subscriptionPlan={subscriptionPlan} />
-// }
-
-// export default Page
+export default Page
